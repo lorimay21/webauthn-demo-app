c566a842c28171bb7d24803455d177d7c7604cb1
Lori May De Guzman <dlorimay21@gmail.com>
2021-10-18 16:26:47 +0800

    Added router

---
+++ src/router.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import store from './store';
+import Top from '@/pages/Top';
+import Login from '@/pages/Login';
+import Register from '@/pages/Register';
+
+Vue.use(Router);
+
+console.log(Top);
+
+const routes = [
+  {
+    path: '/',
+    name: 'top',
+    component: Top
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+];
+
+const router = new Router({
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+  store.commit('navigator/reset', to.matched.map(m => m.components.default));
+  next();
+});
+
+export default router;
