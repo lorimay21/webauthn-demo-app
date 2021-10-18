42eded6e7ce2c3103729338c3e66485e7e7083c9
Lori May De Guzman <dlorimay21@gmail.com>
2021-10-18 16:26:11 +0800

    Added top, login and register pages

---
+++ src/pages/Top.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-ons-page>
+    <v-ons-toolbar></v-ons-toolbar>
+
+    <section>
+      <div class="container">
+        <div class="text-center m-5">
+          <h1>{{ title }}</h1>
+        </div>
+
+        <div class="pt-4">
+          <div class="mx-5 mb-2 text-center">
+            <b-button variant="primary" class="w-50" @click="login"
+              >Login</b-button
+            >
+          </div>
+          <div class="mx-5 mb-2 text-center">
+            <b-button variant="primary" class="w-50" @click="register"
+              >Register</b-button
+            >
+          </div>
+          <div class="mx-5 mb-2 text-center">
+            <b-button variant="primary" class="w-50" @click="reissue"
+              >Re-issue</b-button
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+  </v-ons-page>
+</template>
+<script>
+export default {
+  name: "top-page",
+  data() {
+    return {
+      title: "WebAuthn Demo",
+    };
+  },
+  methods: {
+    /**
+     * Redirect to Login page
+     */
+    login() {
+      this.$router.push({ name: "login" });
+    },
+
+    /**
+     * Redirect to Register page
+     */
+    register() {
+      this.$router.push({ name: "register" });
+    },
+
+    /**
+     * Redirect to Reissue page
+     */
+    reissue() {
+      this.$ons.notification.alert("Temporarily unavailable.");
+    },
+  },
+};
+</script>
