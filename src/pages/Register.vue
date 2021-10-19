3fa894514ea45161df0ccd005d70d3af4633e353
Lori May De Guzman <dlorimay21@gmail.com>
2021-10-19 09:25:50 +0800

    Webauthn eemo Registration page

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -4,52 +4,72 @@
 
     <section>
       <div class="container">
-        <h1>Vue Webauthn demo</h1>
-        <div>
-          <div>
-            <label>
-              Username *
-              <input v-model="inputData.username" />
-            </label>
+        <div class="text-center m-5">
+          <h1 class="font-weight-bold">{{ title }}</h1>
+        </div>
+        <div class="p-4">
+          <b-form @submit.stop.prevent>
+            <div class="mb-3">
+              <label for="feedback-name">
+                Name <span class="text-danger">*</span>
+              </label>
+              <b-form-input
+                v-model="inputData.name"
+                id="feedback-name"
+              ></b-form-input>
+              <b-form-invalid-feedback>
+                {{ errors.name }}
+              </b-form-invalid-feedback>
+            </div>
+            <div class="mb-3">
+              <label for="feedback-email">
+                Email Address <span class="text-danger">*</span>
+              </label>
+              <b-form-input
+                v-model="inputData.emailAddress"
+                id="feedback-email"
+              ></b-form-input>
+              <b-form-invalid-feedback>
+                {{ errors.emailAddress }}
+              </b-form-invalid-feedback>
+            </div>
+          </b-form>
+          <div class="pt-4 text-center">
+            <b-button
+              :disabled="inputData.name && inputData.emailAddress == false"
+              variant="primary"
+              class="w-50 my-1"
+              @click="register"
+            >
+              Register
+            </b-button>
+            <b-button variant="danger" class="w-50 my-1" @click="cancel">
+              Cancel
+            </b-button>
           </div>
-          <div>
-            <label>
-              Email *
-              <input v-model="inputData.emailAddress" />
-            </label>
-          </div>
-          <hr />
-          <button
-            :disabled="inputData.username && inputData.emailAddress == false"
-            @click="register"
-          >
-            Register
-          </button>
         </div>
 
-        <div v-if="userDetails">
-          <button @click="authenticate">Sign in</button>
-        </div>
-
-        <div class="output">
-          <pre
-            v-if="formattedUserDetails"
-          ><code>{{ formattedUserDetails }}</code></pre>
-          <pre v-if="formattedSavedCred"><!--
-				--><span>// send this to server to verify and enroll the key</span>
-<!--			--><code>{{ formattedSavedCred }}</code>
-			</pre>
-          <pre v-if="formattedAssertion"><!--
-				--><span>// send this to server to verify a sign-in</span>
-<!--			--><code>{{ formattedAssertion }}</code>
-			</pre>
-        </div>
+        <!-- <div class="output">
+          <pre v-if="formattedUserDetails">
+            <code>{{ formattedUserDetails }}</code>
+          </pre>
+          <pre v-if="formattedSavedCred">
+            <span>// send this to server to verify and enroll the key</span>
+            <code>{{ formattedSavedCred }}</code>
+          </pre>
+          <pre v-if="formattedAssertion">
+            <span>// send this to server to verify a sign-in</span>
+            <code>{{ formattedAssertion }}</code>
+          </pre>
+        </div> -->
       </div>
     </section>
   </v-ons-page>
 </template>
 
 <script>
+import config from "@/config.js";
+import formRequests from "@/plugins/FormRequests";
 import * as defaults from "@/plugins/Defaults";
 import {
   generateId,
@@ -59,120 +79,124 @@ import {
   encodeAssertion,
 } from "@/plugins/Helpers";
 
-console.log(defaults);
-
 export default {
   name: "register-page",
   components: {},
   data: () => ({
+    title: "Registration",
     inputData: {
-      username: "",
+      name: "",
       emailAddress: "",
-      relyingParty: {
-        name: "Webauthn Demo App",
-        id: "lorimay21.github.io",
-      },
-      attachment: "any",
-      attestation: "none",
     },
+    relyingParty: {
+      name: "Webauthn Demo App",
+      id: config.domain,
+    },
+    attachment: "any",
+    attestation: "none",
     userDetails: null,
     savedCred: null,
     assertion: null,
+    errors: {
+      name: "",
+      emailAddress: "",
+    },
   }),
   computed: {
-    formattedUserDetails() {
-      if (!this.userDetails) return null;
-      return {
-        ...this.userDetails,
-        id: ab2b64(this.userDetails.id),
-      };
-    },
-    formattedSavedCred() {
-      if (!this.savedCred) return null;
-      return encodeCredential(this.savedCred);
-    },
-    formattedAssertion() {
-      if (!this.assertion) return null;
-      return encodeAssertion(this.assertion);
-    },
+    // formattedUserDetails() {
+    //   if (!this.userDetails) return null;
+    //   return {
+    //     ...this.userDetails,
+    //     id: ab2b64(this.userDetails.id),
+    //   };
+    // },
+    // formattedSavedCred() {
+    //   if (!this.savedCred) return null;
+    //   return encodeCredential(this.savedCred);
+    // },
+    // formattedAssertion() {
+    //   if (!this.assertion) return null;
+    //   return encodeAssertion(this.assertion);
+    // },
   },
   methods: {
+    /**
+     * Register new user
+     */
     async register() {
-      const publicKey = {
-        rp: this.inputData.relyingParty,
-        pubKeyCredParams: defaults.pubKeyCredParams,
-        timeout: 60 * 1000,
+      // Validate inputs
+      let [hasError, errors] = formRequests.validateRegistrationForm(
+        this.inputData
+      );
 
-        attestation: this.inputData.attestation,
+      // Clear form
+      this.clearForm();
 
-        // This would allow restricting devices to e.g. touchid or u2f key but it doesn't work consistently at the moment (2019-01-20)
-        authenticatorSelection:
-          this.inputData.attachment === "any"
-            ? undefined
-            : {
-                ...defaults.authenticatorSelection,
-                authenticatorAttachment: this.inputData.attachment,
-              },
+      // Render validation errors
+      if (hasError) {
+        return (this.errors = errors);
+      }
 
-        // cryptographic challenge from server
-        challenge: generateChallenge(),
-
-        // user details from server post account creation
-        user: {
-          id: generateId(),
-          name: this.inputData.emailAddress,
-          displayName: this.inputData.username,
-        },
-      };
-
-      console.log({ publicKey });
-
-      // register / create a new credential
       try {
+        // Initiliaze public key configurations
+        const publicKey = {
+          rp: this.relyingParty,
+          pubKeyCredParams: defaults.pubKeyCredParams,
+          timeout: 60 * 1000,
+          attestation: this.attestation,
+          authenticatorSelection:
+            this.attachment === "any"
+              ? undefined
+              : {
+                  ...defaults.authenticatorSelection,
+                  authenticatorAttachment: this.attachment,
+                },
+          challenge: generateChallenge(),
+          user: {
+            id: generateId(),
+            name: this.inputData.emailAddress,
+            displayName: this.inputData.name,
+          },
+        };
+
+        console.log({ publicKey });
+        console.log(JSON.stringify({ publicKey }));
+
+        // Register new user
         const cred = await navigator.credentials.create({ publicKey });
+
+        // Set credentials
         this.savedCred = cred;
         this.userDetails = publicKey.user;
+
         console.log(`Credential obtained`, this.savedCred);
-      } catch (e) {
-        console.error(e.message);
+      } catch (error) {
+        console.error(error.message);
       }
     },
-    async authenticate() {
-      if (!this.savedCred) {
-        alert("You must register first");
-        return;
-      }
 
-      const publicKey = {
-        rp: this.inputData.relyingParty,
-        pubKeyCredParams: defaults.pubKeyCredParams,
-        attestation: defaults.attestation,
-        timeout: 60 * 1000,
+    /**
+     * Cancel registration process
+     */
+    cancel() {
+      this.$router.push({ name: "top" });
+    },
 
-        // cryptographic challenge from server
-        challenge: generateChallenge(),
-
-        allowCredentials: [
-          {
-            // user's credential id and settings from server
-            id: this.savedCred.rawId,
-            // transports: defaults.transports,
-            type: defaults.credentialType,
-          },
-        ],
+    /**
+     * Clear form
+     */
+    clearForm() {
+      this.errors = {
+        name: "",
+        emailAddress: "",
       };
-
-      console.log({ publicKey });
-      console.log(JSON.stringify({ publicKey }));
-
-      try {
-        const assertion = await navigator.credentials.get({ publicKey });
-        console.log(`Assertion obtained`, assertion);
-        this.assertion = assertion;
-      } catch (e) {
-        console.error(e.message);
-      }
     },
   },
 };
 </script>
+
+<style scoped>
+.invalid-feedback {
+  display: block !important;
+}
+</style>
